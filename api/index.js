6b733547f8d2a5ff47d29e767e4de4a9cc9b99af
unknown <mdsabbirrahman2025@gmail.com>
2024-04-27 00:28:29 +0600

    new

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -30,12 +30,12 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing",listingRouter );
 
 
-app.use(express.static(path.join(_dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(_dirname,'client', 'dist', 'index.html'));
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
